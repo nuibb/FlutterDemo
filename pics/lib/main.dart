d679b8042d6b342facaee3d12e2689ce51c01ff1
Nurul Islam <nislam@medavante.com>
2020-07-19 16:35:41 +0600

    Project pics added to download pics from API request

---
+++ pics/lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:pics/src/MyApp.dart';
+
+void main() {
+  runApp(MyApp());
+}
---
+++ pics/lib/src/MyApp.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' show get;
+import 'models/image_model.dart';
+import 'widgets/image_list.dart';
+import 'dart:convert';
+
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return AppState();
+  }
+  
+}
+
+class AppState extends State<MyApp> {
+  int counter = 0;
+  List<ImageModel> images = [];
+
+  void fetchImage() async {
+    counter++;
+    var response = await get('https://jsonplaceholder.typicode.com/photos/$counter');
+    var imageModel = ImageModel.fromJson(json.decode(response.body));
+    setState(() {
+      images.add(imageModel);
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return MaterialApp(
+      home: Scaffold(
+        body: ImageList(images),
+        floatingActionButton: FloatingActionButton(
+          child: Icon(Icons.add),
+          onPressed: fetchImage,
+        ),
+        appBar: AppBar(
+          title: Text('Lets see some images!'),
+        ),
+      ),
+    );
+  }
+}
